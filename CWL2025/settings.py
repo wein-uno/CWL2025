4ab699faa286dcee422d55493fee8f7c0c598bc8
Erik Weinmeister <eweinmeister@unomaha.edu>
2024-12-06 14:07:27 -0600

    not sure where I am at...was sick for a few days. should I restart or fork?

--- CWL2025/settings.py
+++ CWL2025/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'comicapi',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+       ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  # Replace with your React app's URL
 ]
 
 ROOT_URLCONF = 'CWL2025.urls'
